24b0dfcc737823786d7115fa741a0cbe77f36622
zhangqiang <44993003+JACK-ZHANG-coming@users.noreply.github.com>
2022-07-01 15:16:37 +0800

    添加路由权限

--- config/routes.ts
+++ config/routes.ts
@@ -17,17 +17,17 @@
     name: '权益平台管理',
     path:'/mallMenu',
     icon: 'shop',
-    // component: './TestPageOne',
+    // access: 'normalRouteFilter',
     routes:[
       {
         name:'微信平台管理',
         path:'/mallMenu/companyManager',
-        // icon:'setting',
+        // access: 'normalRouteFilter',
         routes:[
           {
             name:'公众号管理',
             path:'/mallMenu/companyManager/weChartManager',
-            // icon:'setting',
+            // access: 'normalRouteFilter',
             component: './CompanyManager/WeChartManager',
           }
         ]
@@ -35,99 +35,124 @@
       {
         name:'积分管理',
         path:'/mallMenu/corpPointsPool',
+        // access: 'normalRouteFilter',
+        access: 'adminRouteFilter',
         routes:[
           {
             name:'积分账户',
             path:'/mallMenu/corpPointsPool/pointAccounts',
             component: './CorpPointsPool/PointAccounts',
+            // access: 'normalRouteFilter',
+            access: 'adminRouteFilter',
           },
           {
             name:'积分分配',
             path:'/mallMenu/corpPointsPool/distPoints',
             component: './CorpPointsPool/DistPoints',
+            // access: 'normalRouteFilter',
+            access: 'adminRouteFilter',
           },
         ]
       },
       {
         name:'积分流水',
         path:'/mallMenu/flows',
+        // access: 'normalRouteFilter',
+        access: 'adminRouteFilter',
         routes:[
           {
             name:'企业积分流水',
             path:'/mallMenu/flows/tenant',
             component: './Flows/Tenant',
+            // access: 'normalRouteFilter',
+            access: 'adminRouteFilter',
           },
           {
             name:'用户积分流水',
             path:'/mallMenu/flows/customers',
             component: './Flows/Customers',
+            // access: 'normalRouteFilter',
+            access: 'adminRouteFilter',
           },
         ]
       },
       {
         name:'订单中心',
         path:'/mallMenu/orders',
+        // access: 'normalRouteFilter',
         routes:[
           {
             name:'订单管理',
             path:'/mallMenu/orders/list',
             component: './Orders/List',
+            // access: 'normalRouteFilter',
           },
           {
             name:'供货商订单管理',
             path:'/mallMenu/orders/supplier',
             component: './Orders/Supplier',
+            // access: 'normalRouteFilter',
           },
           {
             name:'实物发货',
             path:'/mallMenu/orders/deliver',
             component: './Orders/Deliver',
+            // access: 'normalRouteFilter',
           },
         ]
       },
       {
         name:'商品中心',
         path:'/mallMenu/commodity',
+        // access: 'normalRouteFilter',
         routes:[
           {
             name:'类目管理',
             path:'/mallMenu/commodity/category',
             component: './Commodity/Category',
+            // access: 'normalRouteFilter',
           },
           {
             name:'品牌管理',
             path:'/mallMenu/commodity/brand',
             component: './Commodity/Brand',
+            // access: 'normalRouteFilter',
           },
           {
             name:'产品管理',
             path:'/mallMenu/commodity/product',
             component: './Commodity/Product',
+            // access: 'normalRouteFilter',
           },
           {
             name:'商品管理',
             path:'/mallMenu/commodity/detail',
             component: './Commodity/Detail',
+            // access: 'normalRouteFilter',
           },
           {
             name:'属性管理',
             path:'/mallMenu/commodity/attrs',
             component: './Commodity/Attrs',
+            // access: 'normalRouteFilter',
           },
           {
             name:'商品生成列表',
             path:'/mallMenu/commodity/speedGenerator/list',
             component: './Commodity/SpeedGenerator',
+            // access: 'normalRouteFilter',
           },
           {
             name:'商品移植',
             path:'/mallMenu/commodity/transplant',
             component: './Commodity/Transplant',
+            // access: 'normalRouteFilter',
           },
           {
             name:'供货商商品移植',
             path:'/mallMenu/commodity/supplier/transplant',
             component: './Commodity/SupplierTransplant',
+            // access: 'normalRouteFilter',
           },
         ]
       },
@@ -137,18 +162,20 @@
     name: '营销平台管理',
     path: '/test2',
     icon: 'smile',
+    // access: 'normalRouteFilter',
     routes:[
       {
         path: '/test2/test2-1',
         name: 'test2-1',
         icon: 'smile',
         component: './TestPageTwo/TestPageTwoOne',
+        // access: 'normalRouteFilter',
       }
     ]
   },
   {
     path: '/',
-    redirect: '/welcome',
+    redirect: '/mallMenu/companyManager/weChartManager',
   },
   {
     component: './404',
--- src/access.ts
+++ src/access.ts
@@ -1,9 +1,45 @@
 /**
  * @see https://umijs.org/zh-CN/plugins/plugin-access
  * */
-export default function access(initialState: { currentUser?: API.CurrentUser } | undefined) {
-  const { currentUser } = initialState ?? {};
+
+/**
+ * 根据返回是否管理员状态值配置路由菜单权限
+ * @param initialState 
+ * @returns 
+ */
+export default function access(initialState: {
+  currentUser?: API.CurrentUser;
+  isAdmin?: boolean;
+  hasRoutes?: string[];
+} | undefined) {
+  let { currentUser, isAdmin = false, hasRoutes = [] } = initialState ?? {};
+  // 配置是否管理员权限
+  isAdmin = true;
+  // isAdmin = false;
   return {
     canAdmin: currentUser && currentUser.access === 'admin',
+    adminRouteFilter: () => isAdmin, // 只有管理员可访问
   };
 }
+
+/**
+ * 根据返回的路由菜单列表配置路由菜单权限
+ * @param initialState 
+ * @returns 
+ */
+// export default function access(initialState: {
+//   currentUser?: API.CurrentUser;
+//   isAdmin?: boolean;
+//   hasRoutes?: string[];
+// } | undefined) {
+//   let { currentUser, isAdmin = false, hasRoutes = [] } = initialState ?? {};
+//   // 配置是否管理员权限
+//   isAdmin = true;
+//   // 配置路由权限
+//   hasRoutes = ['权益平台管理', '微信平台管理', '公众号管理', '商品中心', '类目管理', '品牌管理', '产品管理', '商品管理', '属性管理', '商品生成列表']
+//   return {
+//     canAdmin: currentUser && currentUser.access === 'admin',
+//     // adminRouteFilter: () => isAdmin, // 只有管理员可访问
+//     normalRouteFilter: (route) => hasRoutes.includes(route.name), // initialState 中包含了的路由才有权限访问
+//   };
+// }
